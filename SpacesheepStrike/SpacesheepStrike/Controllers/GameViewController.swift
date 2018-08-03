573dcddc35a545041973892ee307e101abc1390d
Felipe Izepe <felipeizepe@gmail.com>
2018-08-03 14:28:45 -0300

    Push Inicial com projeto base, nave e skybox aparecendo

---
+++ SpacesheepStrike/SpacesheepStrike/Controllers/GameViewController.swift
@@ -0,0 +1,97 @@
+//
+//  GameViewController.swift
+//  SpacesheepStrike
+//
+//  Created by Felipe Izepe on 03/08/18.
+//  Copyright © 2018 Felipe Izepe. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// create a new scene
+		let scene = SCNScene(named: "GameScene.scn")!
+		let camera = scene.rootNode.childNode(withName: "camera", recursively: true)!
+		
+		// retrieve the SCNView
+		let scnView = self.view as! SCNView
+		
+		// set the scene to the view
+		scnView.scene = scene
+		scnView.pointOfView = camera
+		
+		// allows the user to manipulate the camera
+		scnView.allowsCameraControl = false
+		
+		// show statistics such as fps and timing information
+		scnView.showsStatistics = true
+		
+		// add a tap gesture recognizer
+		let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+		scnView.addGestureRecognizer(tapGesture)
+		
+		scnView.backgroundColor  = SKColor.white
+		
+	}
+	
+	
+	@objc
+	func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+		// retrieve the SCNView
+		let scnView = self.view as! SCNView
+		
+		// check what nodes are tapped
+		let p = gestureRecognize.location(in: scnView)
+		let hitResults = scnView.hitTest(p, options: [:])
+		// check that we clicked on at least one object
+		if hitResults.count > 0 {
+			// retrieved the first clicked object
+			let result = hitResults[0]
+			
+			// get its material
+			let material = result.node.geometry!.firstMaterial!
+			
+			// highlight it
+			SCNTransaction.begin()
+			SCNTransaction.animationDuration = 0.5
+			
+			// on completion - unhighlight
+			SCNTransaction.completionBlock = {
+				SCNTransaction.begin()
+				SCNTransaction.animationDuration = 0.5
+				
+				material.emission.contents = UIColor.black
+				
+				SCNTransaction.commit()
+			}
+			
+			material.emission.contents = UIColor.red
+			
+			SCNTransaction.commit()
+		}
+	}
+	
+	override var shouldAutorotate: Bool {
+		return false
+	}
+	
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+	
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		if UIDevice.current.userInterfaceIdiom == .phone {
+			return .allButUpsideDown
+		} else {
+			return .all
+		}
+	}
+	
+}
